2d16ca8eba04df15347b2a942d63eb7c89a69f57
Saurav Khairnar <124131200+sauravkhairnar@users.noreply.github.com>
2023-04-19 10:22:00 +0530

    first commit

---
+++ SpringWebAppUsingMVC/src/main/java/org/tnsif/springwebmvc/SpringWebAppUsingMvcApplication.java
@@ -0,0 +1,18 @@
+package org.tnsif.springwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringWebAppUsingMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppUsingMvcApplication.class, args);
+		
+	
+	}
+
+}
+
+ 
---
+++ SpringWebAppUsingMVC/src/main/webapp/home.jsp
@@ -0,0 +1,7 @@
+<html>
+   <head><title>Hello World</title></head>
+   
+   <body>
+      <h1>springboot is a standalone application and it requires embedded server like tomcat, jetty,etc to test on theserver</h1>
+   </body>
+</html>
